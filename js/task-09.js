37315a074fce4a0d12fc6c8acb9faaec82a774be
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-01-21 12:46:34 +0100

    task-09-done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const span = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+const background = document.body;
+
+button.addEventListener("click", changeColor);
+
+function changeColor() {
+  background.style.backgroundColor = getRandomHexColor();
+}
